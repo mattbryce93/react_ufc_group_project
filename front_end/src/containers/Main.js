379f9271d952660c34eb5e9d75170d323b9e948e
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-08-19 17:20:34 +0100

    Added filtering to first API call by fighter_status === Active

--- front_end/src/containers/Main.js
+++ front_end/src/containers/Main.js
@@ -3,7 +3,7 @@ import ListContainer from './ListContainer';
 import Title from '../components/Title'
 import NavBar from '../components/NavBar'
 import TeamContainer from './TeamContainer'
-
+import _ from 'lodash';
 
 
 class Main extends Component{
@@ -11,9 +11,9 @@ class Main extends Component{
     super(props);
     this.state = {
       allFighters: []
+    }
+    this.apiCall = this.apiCall.bind(this);
   }
-  this.apiCall = this.apiCall.bind(this);
-}
 
   componentDidMount(){
     this.apiCall();
@@ -22,20 +22,26 @@ class Main extends Component{
   apiCall() {
     fetch('http://localhost:3001/api/fighters')
     .then(response => response.json())
-    .then(fighters => this.setState({allFighters: fighters}))
-  }
-
-  render(){
-    return(
-      <React.Fragment>
-        <NavBar/>
-        <Title/>
-        {/* <p>Main</p> */}
-        <TeamContainer/>
-        <ListContainer allFighters={this.state.allFighters}/>
-      </React.Fragment>
+    // .then(
+    .then(fighters => this.setState
+      ({
+        allFighters: fighters
+        .filter(fighter => fighter.fighter_status === 'Active')
+      })
     )
   }
+
+render(){
+  return(
+    <React.Fragment>
+      <NavBar/>
+      <Title/>
+      {/* <p>Main</p> */}
+      <TeamContainer/>
+      <ListContainer allFighters={this.state.allFighters}/>
+    </React.Fragment>
+  )
+}
 }
 
 export default Main;
