75e4be4ae65d1a9b16dc7e3a9aa8bab7b2e8d349
Roddy Daly <roddy_daly110@hotmail.com>
2018-08-20 21:40:39 +0100

    ammended container props after merge with styling branch

--- front_end/src/components/TeamListItem.js
+++ front_end/src/components/TeamListItem.js
@@ -16,7 +16,7 @@ const TeamListItem = (props) => {
         <div className="teamListItem">
           {thumbnailImg}
           {prettyName}
-          <DeleteOneFromTeamButton/>
+          <DeleteOneFromTeamButton id={props.fighter.id}/>
                 <div className="teamListItem-divider"></div>
         </div>
 
