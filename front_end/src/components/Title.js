86acff270ab39693c48695083c7d9c330ec3efdc
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-08-21 19:31:12 +0100

    Some work with colour and background

--- front_end/src/components/Title.js
+++ front_end/src/components/Title.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Title.css'
 
 const Title = () => (
   <div className="title-container">
-    <img src="images/ufc9.jpg"/>
+    {/* <img src="images/ufc9.jpg"/> */}
+    <h1>FanUFC</h1>
   </div>
 )
 export default Title;
