edf54803b2ecaa8c4c68346ea0f8149f5c4dfbc5
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-08-22 21:33:55 +0100

    Added alternative logo version in comments

--- front_end/src/components/Title.js
+++ front_end/src/components/Title.js
@@ -6,7 +6,10 @@ const Title = () => (
     {/* <img src="images/ufc9.jpg"/> */}
     {/* <img className="title-img" src="https://i.imgur.com/VhgCXeJ.png" alt="ufclogo"/> */}
     {/* <img className="title-img" src="https://i.imgur.com/JLIiaGA.png" alt="ufclogo"/> */}
+    {/* BEST 2 v */}
     <img className="title-img" src="https://i.imgur.com/i01XUkU.png" alt="ufclogo"/>
+    {/* <img className="title-img" src="https://i.imgur.com/ATMrmSC.png" alt="ufclogo"/> */}
+
     {/* <h1 className="fan">fan</h1><h1 className="ufc">UFC</h1> */}
   </div>
 )
